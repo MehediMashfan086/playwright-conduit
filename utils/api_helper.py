c3ac4ac6bf3520b6d685a490e4f13843975cdbd8
Mehedi Hasan <mehedihasan62086@gmail.com>
2025-11-14 01:10:26 +0600

    remove unused functions

--- utils/api_helper.py
+++ utils/api_helper.py
@@ -3,16 +3,6 @@ import requests
 BASE_URL = "https://conduit-api.bondaracademy.com/api"
 
 
-def get_user_token(email, password):
-    # Log in and return the user's authentication token.
-
-    url = f"{BASE_URL}/users/login"
-    payload = {"user": {"email": email, "password": password}}
-    res = requests.post(url, json=payload)
-    res.raise_for_status()
-    return res.json()["user"]["token"]
-
-
 def create_article(token, title, description, body, tags):
     # Create a new article using the Conduit API.
     url = f"{BASE_URL}/articles"
